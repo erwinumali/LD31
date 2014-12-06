f34c23d5a4b6af9f2c6b17bbf7bee344ca8bbfa3
Erwin Umali <kazukishida@gmail.com>
2014-12-07 01:47:08 +0900

    implement basic bullet shooting

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	public bool isFiring;
+	public float damage = 1.0f;
+	
+	public Vector3 direction = Vector3.zero;
+	public Quaternion rotationSpeed = Quaternion.identity;
+	public float speed = 30.0f;
+	
+	public float maxDuration = 3.0f;
+	public float timeAlive;
+
+	// Use this for initialization
+	void Start () {
+		isFiring = false;
+		damage = 1.0f;
+		timeAlive = 0.0f;
+		transform.rotation = Random.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isFiring){
+			//Debug.Log(direction + ", " + Input.mousePosition);
+			timeAlive += Time.deltaTime;
+			transform.position = transform.position + ((direction * speed) * Time.deltaTime);
+			//transform.rotation = Quaternion.Dot(transform.rotation, rotationSpeed);
+			if(timeAlive > maxDuration){
+				Reset ();
+			}
+		}
+	}
+	
+	public void Reset(){
+		isFiring = false;
+		timeAlive = 0.0f;
+		transform.position = new Vector3(0, 20.0f, 0);
+		transform.rotation = Random.rotation;
+	}
+}
--- Assets/Scripts/PlayerTurretManager.cs
+++ Assets/Scripts/PlayerTurretManager.cs
@@ -1,18 +1,46 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class PlayerTurretManager : MonoBehaviour {
 
+	public enum BulletType {basic};
+
+	public GameObject bullet;
+	public float bullet1Cooldown = 0.03f;
+
+	private GameObject[] bulletArray;
+	private int bulletInstances = 100;
+
+	private float bullet1Timer = 0.0f;
+
 	private Vector3 pos;
 	private Vector3 dir;
 	private float angle;
+	
+	private GameObject currentBullet;
+	private Bullet currentBulletComponent;
+	
+	private int bulletCounter = 0;
 	// Use this for initialization
 	void Start () {
+		if(bullet == null){
+			throw new UnityException("no bullet");
+		} else if(bullet.GetComponent<Bullet>() == null){
+			throw new UnityException("no bullet component in prefab!");
+		} else {
+			bulletArray = new GameObject[bulletInstances];
+			for(int i = 0; i < bulletInstances; i++){
+				bulletArray[i] = (GameObject) GameObject.Instantiate(bullet, new Vector3(0.0f, 13.0f, 0.0f), Quaternion.identity);
+			}
+		}
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		bullet1Timer += Time.deltaTime;
+		
+		// turret rotation
 		//Debug.Log (Input.mousePosition + ", " + Camera.main.pixelWidth + "," + Camera.main.pixelHeight);
 		if(	Input.mousePosition.x < Camera.main.pixelWidth && 
 			Input.mousePosition.y < Camera.main.pixelHeight){
@@ -23,6 +51,35 @@ public class PlayerTurretManager : MonoBehaviour {
 			transform.rotation = Quaternion.AngleAxis(angle, Vector3.down); 
 		}
 		
+		if(Input.GetMouseButton(0) || Input.GetKey(KeyCode.Space)){
+			if(bullet1Timer >= bullet1Cooldown){
+				Fire(BulletType.basic);
+				bullet1Timer = 0.0f;
+			}
+		}
+	}
+	
+	private void Fire(BulletType bullet){
+		if(bullet.Equals(BulletType.basic)){
+			Fire ();
+		}
+	}
+	
+	private void Fire(){
+		currentBullet = bulletArray[bulletCounter % bulletInstances];
+		currentBulletComponent = bulletArray[bulletCounter % bulletInstances].GetComponent<Bullet>();
 		
+		while(currentBulletComponent.isFiring){
+			bulletCounter += 1;
+			if(bulletCounter > 65536){
+				bulletCounter = 0;
+			}
+			currentBullet = bulletArray[bulletCounter % bulletInstances];
+			currentBulletComponent = bulletArray[bulletCounter % bulletInstances].GetComponent<Bullet>();
+		} 
+		
+		currentBullet.transform.position = this.transform.position;
+		currentBulletComponent.direction = Vector3.Normalize(new Vector3(dir.x, 0.0f, dir.y));
+		currentBulletComponent.isFiring = true;
 	}
 }
